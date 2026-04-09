9f89d4317c3c17db49691fe941ad34bd1ea04136
EslamKamel89 <eslam_kamel_89@hotmail.com>
2026-04-09 05:38:06 +0200

    feat(cart): add cart serializers and introduce CartData schema for structured cart representation

--- app/cart/schemas.py
+++ app/cart/schemas.py
@@ -2,8 +2,10 @@ from typing import TypedDict
 
 
 class ProductData(TypedDict):
-    session_id: str
     product_id: int
     name: str
     quantity: int
     price: str
+
+
+CartData = dict[str, ProductData]
--- app/cart/serializers.py
+++ app/cart/serializers.py
@@ -4,3 +4,18 @@ from rest_framework import serializers
 class AddToCartSerializer(serializers.Serializer):
     product_id = serializers.IntegerField(min_value=1)
     quantity = serializers.IntegerField(min_value=1, default=1)
+
+
+class CartItemSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+    name = serializers.CharField()
+    price = serializers.CharField()
+    quantity = serializers.IntegerField()
+
+
+class CartSerializer(serializers.Serializer):
+    items = CartItemSerializer(many=True)
+
+
+class RemoveFromCartSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
